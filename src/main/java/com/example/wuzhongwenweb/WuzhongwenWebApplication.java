e49c1365744dc964db18a919b303b2d470c7b897
wzw <2537366933@qq.com>
2022-11-05 00:14:23 +0800

    提交测试

---
+++ src/main/java/com/example/wuzhongwenweb/WuzhongwenWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.wuzhongwenweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzhongwenWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuzhongwenWebApplication.class, args);
+    }
+
+}
